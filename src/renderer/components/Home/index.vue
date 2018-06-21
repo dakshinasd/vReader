269c507c94e18fb613e7d94927703e049ef4e333
Dakshina Wijesuriya <me@dakshinasd.com>
2018-06-21 09:04:35 +0530

    Addded basic styling and layout changes

---
+++ src/renderer/components/Home/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <div class="sidebar vr-container">
+          <button class="vr-btn normal">Hello</button>
+        </div>
+        <div class="main-container vr-container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'home',
+    components: {  },
+    methods: {
+      
+    }
+  }
+</script>
